0a4bc10ba3dee484b5dacbd97b87f86ea9d76a6f
dasxophi <75560513+dasxophi@users.noreply.github.com>
2024-09-26 08:49:32 +0900

    add:migrations

---
+++ database/migrations/2024_09_22_124225_create_player_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('player', function (Blueprint $table) {
+            $table->string('user_id')->primary();
+            $table->integer('level');
+            $table->bigInteger('exp');
+            $table->bigInteger('money');
+            $table->unsignedBigInteger('credits');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('player');
+    }
+};
---
+++ database/migrations/2024_09_22_135734_create_player_card_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('player_card', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->unsignedInteger('card_id');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('player_card');
+    }
+};
