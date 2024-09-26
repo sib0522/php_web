37c90a22409582b67140f00b261f0823319cb616
dasxophi <75560513+dasxophi@users.noreply.github.com>
2024-09-26 09:02:16 +0900

    add:master追加

---
+++ master/CardMaster.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Master;
+
+use App\Core\Entities\Master\Card;
+
+final class CardMaster extends MasterBase {
+    public function __construct()
+    {
+        parent::__construct("CardMaster");
+    }
+
+    public function getById(string $Id) {
+        foreach ($this->datas as $data) {
+            var_dump($data);
+            if ($data['Id'] == $Id) {
+                return new Card
+                (
+                    $this->withType('Id', $data['Id']), 
+                    $this->withType('CardName', $data['CardName']), 
+                    $this->withType('ImageURL', $data['ImageURL']), 
+                    $this->withType('Rarity', $data['Rarity'])
+                );
+            }
+        }
+        return null;
+    }
+}
---
+++ master/GachaMaster.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Master;
+
+use App\Core\Entities\Master\Gacha;
+
+final class GachaMaster extends MasterBase {
+    public function __construct()
+    {
+        parent::__construct("GachaMaster");
+    }
+
+    public function getById(string $Id) {
+        foreach ($this->datas as $data) {
+            if ($data['Id'] === $Id) {
+                $gacha = new Gacha
+                (
+                    $this->withType('Id', $data['Id']), 
+                    $this->withType('GachaName', $data['GachaName']), 
+                    $this->withType('TargetCardIds', $data['TargetCardIds']), 
+                    $this->withType('PickupCardIds', $data['PickupCardIds'])
+                );
+                return $gacha;
+            }
+        }
+        return null;
+    }
+}
