c565b222f70afcb71c7b516fddcd184510a4e4ec
dasxophi <75560513+dasxophi@users.noreply.github.com>
2024-09-27 07:39:56 +0900

    update:entities

--- app/Core/Entities/Master/Card.php
+++ app/Core/Entities/Master/Card.php
@@ -3,12 +3,12 @@
 namespace App\Core\Entities\Master;
 
 final class Card {
-    public readonly int $Id;
+    public readonly string $Id;
     public readonly string $CardName;
     public readonly string $ImageURL;
     public readonly int $Rarity;
 
-    public function __construct(int $id, string $cardName, string $imageUrl, int $rarity)
+    public function __construct(string $id, string $cardName, string $imageUrl, int $rarity)
     {
         $this->Id = $id;
         $this->CardName = $cardName;
